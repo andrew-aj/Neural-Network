d172d40154e1567370a982ae46cddb415e46a7c2
Andrew Knee <andrew.knee.j@gmail.com>
2018-10-21 00:10:09 -0400

    Fixed the matrix math

---
+++ Neural-Networl/Neural-Network/vmath.cpp
@@ -0,0 +1,142 @@
+#include "pch.h"
+#include "vmath.h"
+#include <math.h>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+
+vector<double> vmath::vadd(vector<double> v1, vector<double> v2) {
+
+	vector<double> returnVector = vector<double>();
+
+	for (int x = 0; x < v1.size(); x++) {
+
+		returnVector.push_back(v1[x] + v2[x]);
+
+	}
+
+	return returnVector;
+
+}
+
+vector<double> vmath::vsub(vector<double> v1, vector<double> v2) {
+
+	vector<double> returnVector = vector<double>();
+
+	for (int x = 0; x < v1.size(); x++) {
+
+		returnVector.push_back(v1[x] - v2[x]);
+
+	}
+
+	return returnVector;
+
+}
+
+vector<double> vmath::vmul(vector<double> v1, vector<double> v2) {
+
+	vector<double> returnVector = vector<double>();
+
+	for (int x = 0; x < v1.size(); x++) {
+
+		returnVector.push_back(v1[x] * v2[x]);
+
+	}
+
+	return returnVector;
+
+}
+
+vector<double> vmath::vdiv(vector<double> v1, vector<double> v2) {
+
+	vector<double> returnVector = vector<double>();
+
+	for (int x = 0; x < v1.size(); x++) {
+
+		returnVector.push_back(v1[x] / v2[x]);
+
+	}
+
+	return returnVector;
+
+}
+
+double vmath::vsum(vector<double> vector) {
+
+	double sum = 0;
+	for (double x : vector)
+	{
+		sum += x;
+	}
+	return sum;
+
+}
+
+double vmath::vdot(vector<double> v1, vector<double> v2) {
+
+	vector<double> inputVector = vector<double>();
+	inputVector = vmul(v1, v2);
+
+	return vsum(inputVector);
+
+}
+
+
+
+/*vector<double> vmath::vadd(vector<double> v1, vector<double> v2) {
+
+	vector<double> returnVector;
+
+	transform(v1.begin(), v1.end(), v2.begin(), returnVector.begin(), plus<double>());
+	return returnVector;
+
+}
+
+vector<double> vmath::vsub(vector<double> v1, vector<double> v2) {
+
+	vector<double> returnVector = vector<double>(0);
+
+	transform(v1.begin(), v1.end(), v2.begin(), returnVector.begin(), minus<double>());
+	return returnVector;
+
+}
+
+vector<double> vmath::vmul(vector<double> v1, vector<double> v2) {
+
+	vector<double> returnVector = vector<double>(0);
+
+	transform(v1.begin(), v1.end(), v2.begin(), returnVector.begin(), multiplies<double>());
+	return returnVector;
+
+}
+
+vector<double> vmath::vdiv(vector<double> v1, vector<double> v2) {
+
+	vector<double> returnVector = vector<double>(0);
+
+	transform(v1.begin(), v1.end(), v2.begin(), returnVector.begin(), divides<double>());
+	return returnVector;
+
+}
+
+double vmath::vsum(vector<double> vector)
+{
+	double sum = 0;
+	for (double x : vector)
+	{
+		sum += x;
+	}
+	return sum;
+}
+
+double vmath::vdot(vector<double> v1, vector<double> v2) {
+
+	vector<double> returnVector = vector<double>(0);
+
+	transform(v1.begin(), v1.end(), v2.begin(), returnVector.begin(), multiplies<double>());
+	
+	return vsum(returnVector);
+
+}
+*/
---
+++ Neural-Networl/Neural-Network/vmath.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+class vmath {
+
+	public:
+		vector<double> vadd(vector<double> v1, vector<double> v2);
+		vector<double> vsub(vector<double> v1, vector<double> v2);
+		vector<double> vmul(vector<double> v1, vector<double> v2);
+		vector<double> vdiv(vector<double> v1, vector<double> v2);
+		double vsum(vector<double> vector);
+		double vdot(vector<double> v1, vector<double> v2);
+
+};
+
+
